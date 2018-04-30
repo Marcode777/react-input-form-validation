a5e8353603a496b987aba7bd0c58e6e500cbc034
Marcode777 <marcosebello7@gmail.com>
2018-04-30 06:11:27 -0400

    created validateChange function as test

--- react-input-form-validation/src/components/input_component.js
+++ react-input-form-validation/src/components/input_component.js
@@ -24,6 +24,9 @@ export default class InputComponent extends React.Component{
   onSubmit = (e) => {
     e.preventDefault();
     console.log('onSubmit has been triggered, the new state is:', this.state);
+    console.log('also the window.location.pathname is:', window.location.pathname);
+    if(window.location.pathname.indexOf('/') > -1){
+      console.log('so it has arrived');
     this.setState({
       email: '', 
       emailConfirmation: '',
@@ -34,13 +37,32 @@ export default class InputComponent extends React.Component{
       emailConfirmation: '',
       password: '',
     })
+    var theObject = this.props.onChange({
+      email: '',
+      emailConfirmation: '',
+      password: '',
+    });
+    console.log('theObject is:', theObject);
+   }
   }
+
 
   change = e => {
     this.props.onChange({ [e.target.name]: e.target.value })
     this.setState({
       [e.target.name]: e.target.value,
     });
+  }
+
+  validateChange = e => {
+    const theValue = this.props.onChange({[e.target.name]: e.target.value})
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log('the current state of email is:', this.state.email)
+    if(e.target.value === "key"){
+      alert('match found for key!')
+    }
   }
  
   sampleFunction(){
@@ -59,7 +81,7 @@ export default class InputComponent extends React.Component{
             <input
               name="email" 
               value={this.state.email} placeholder="email" 
-              onChange={e => this.change(e)}
+              onChange={e => this.validateChange(e)}
             />
             <br/>
             <input
